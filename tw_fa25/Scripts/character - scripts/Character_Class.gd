6e7ee977b31beacae9bc87918fe8834cd7a14174
BenWorley-06 <ben.e.worley06@gmail.com>
2025-10-11 09:22:49 -0700

    Added Juice for pickup

--- tw_fa25/Scripts/character - scripts/Character_Class.gd
+++ tw_fa25/Scripts/character - scripts/Character_Class.gd
@@ -1,10 +1,10 @@
 extends Entity
-
 class_name Character
 
-enum Action_State {IDLE,WORKING,CARRIED}
-
 @export var stats: Character_Stats
+@onready var sprite: Sprite2D = $Sprite
+
+enum Action_State {IDLE,WORKING,CARRIED}
 var action_state = Action_State.IDLE
 var current_job=null
 
@@ -14,22 +14,20 @@ var wander_direction: Vector2 = Vector2.ZERO
 var build_timer: float = 0
 
 var selected = false
+var base_scale: Vector2
 
-
+#	--- Main ---
 func _ready():
 	# register self to population manager
+	base_scale = sprite.scale
 	get_node("/root/Game/Managers/PopulationManager").register_character(self)
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_released("left_click"):
-		selected = false;
-		
+		end_grab()
 	if selected:
-		global_position = lerp(global_position, get_global_mouse_position(), 100 * delta);
-		get_node("CollisionShape2D").disabled = true
-		z_index = 10
-	else:
-		get_node("CollisionShape2D").disabled = false
+		global_position = lerp(global_position, get_global_mouse_position(), 40 * delta);
+		
 func _process(delta: float) -> void:
 	
 	match action_state:
@@ -40,6 +38,7 @@ func _process(delta: float) -> void:
 	if not selected:
 		move_and_slide()
 
+#	--- Idle ---
 func idle(delta: float):
 	if wander_timer<=0:
 		print("changed")
@@ -55,10 +54,11 @@ func idle(delta: float):
 func is_idle() -> bool:
 	return action_state==Action_State.IDLE
 	
+#	--- Work ---
 func assign_job(job):
 	current_job = job
 	action_state=Action_State.WORKING
-	
+
 func working(delta:float):
 	match current_job.type:
 		"build":
@@ -88,10 +88,26 @@ func do_build_job(delta: float) -> void:
 			scaffold.complete_building()
 			current_job = null
 			action_state = Action_State.IDLE
-
+#	--- Draging ---
 # drag controller
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if Input.is_action_just_pressed("left_click"):
-		selected = true
+		initiate_grab()
+		
+func initiate_grab():
+	selected = true
+	get_node("CollisionShape2D").disabled = true
+	z_index = 10
+	var tween1 = create_tween()
+	var tween2 = create_tween()
+	tween1.tween_property(sprite, "scale", sprite.scale * 2, 0.4) # scale up over 0.2s
+	tween2.tween_property(sprite, "position:y", sprite.position.y - 100, 0.4) # move sprite up a bit
 	
-	pass # Replace with function body.
+func end_grab():
+	selected = false
+	get_node("CollisionShape2D").disabled = false
+	z_index = 1
+	var tween1 = create_tween()
+	var tween2 = create_tween()
+	tween1.tween_property(sprite, "scale", base_scale, 0.2) # return to normal size over 0.2s
+	tween2.tween_property(sprite, "position:y", 0, 0.2) # move back down
