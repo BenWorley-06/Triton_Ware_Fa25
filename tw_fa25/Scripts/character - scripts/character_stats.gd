2030921b0384b7c4c2b6dd738b4acb55a15bbf49
BenWorley-06 <ben.e.worley06@gmail.com>
2025-10-11 11:14:09 -0700

    Fixed Dragging

--- tw_fa25/Scripts/character - scripts/character_stats.gd
+++ tw_fa25/Scripts/character - scripts/character_stats.gd
@@ -6,9 +6,9 @@ class_name Character_Stats
 var sinner: bool = false
 var fed: bool = true
 
-@export var walk_speed: float = 80
+@export var walk_speed: float = 50
 
-@export var wander_speed: float = 40
+@export var wander_speed: float = 30
 @export var wander_time: float = 5	#How long an idle character wanders in a choosen direction
 
 @export var time_to_harvest: float = 3
