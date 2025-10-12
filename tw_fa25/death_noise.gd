0a1c47c9f5669177bb00d3c615b5407d3fd10f22
BenWorley-06 <ben.e.worley06@gmail.com>
2025-10-11 19:07:49 -0700

    Update death_noise.gd

--- tw_fa25/death_noise.gd
+++ tw_fa25/death_noise.gd
@@ -2,6 +2,5 @@ extends AudioStreamPlayer2D
 
 func _ready() -> void:
 	play()
-	# Wait until the sound finishes before freeing
 	await get_tree().create_timer(stream.get_length()).timeout
 	queue_free()
