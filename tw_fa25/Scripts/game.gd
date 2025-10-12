128f457fcf8c5b6933ee6f56fc4da0d1969562c3
gloomybear300 <s2hill@ucsd.edu>
2025-10-11 17:36:09 -0700

    ui

--- tw_fa25/Scripts/game.gd
+++ tw_fa25/Scripts/game.gd
@@ -8,5 +8,19 @@ func add_character():
 	add_child(character)
 	
 func _process(delta: float) -> void:
+<<<<<<< Updated upstream
 	if Input.is_action_just_pressed("ui_accept"):
 		add_character()
+=======
+	day_timer+=delta
+	if day_timer>=stats.time_in_day:
+		end_day()
+
+func end_day():
+	day+=1
+	day_timer=0
+
+func _input(event):
+	if event.is_action_pressed("escape"): # "quit" is the action defined in Input Map
+		get_tree().change_scene_to_file("res://Scenes/main.tscn")
+>>>>>>> Stashed changes
