d23bdd6df6bb9d6c8c5205a77c88dfae2561efa7
BenWorley-06 <ben.e.worley06@gmail.com>
2025-10-12 18:28:11 -0700

    added fire

---
+++ tw_fa25/fire.gd
@@ -0,0 +1,43 @@
+extends Area2D
+class_name Fire
+
+@export var lifetime: float = 5.0
+@export var spread_time: float = 3
+
+var left_bound=0
+var right_bound=1200
+var up_bound=0
+var lower_bound=700
+
+var timer: float = 0
+var spread_timer: float = 0
+func _ready() -> void:
+	spread_time=spread_time*randf_range(0.5,2)
+	if global_position.x>right_bound or global_position.x<left_bound or global_position.y>lower_bound or global_position.y<up_bound:
+		queue_free()
+
+func _process(delta: float) -> void:
+	timer+=delta
+	spread_timer+=delta
+	if timer>=lifetime:
+		queue_free()
+
+	# occasionally spread
+	if spread_timer>=spread_time:
+		spread_fire()
+		spread_timer=0
+		
+func spread_fire() -> void:
+	var distance = 64  # fixed distance between fires
+	var angle = randf() * TAU  # random direction in radians (0 to 2π)
+
+	var offset = Vector2(cos(angle), sin(angle)) * distance
+	var fire_pos = global_position + offset
+
+	var new_fire = load("res://Scenes/Hazards/fire.tscn").instantiate()
+	new_fire.global_position = fire_pos
+	get_tree().current_scene.add_child(new_fire)
+	
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group('character'):
+		body.enter_volcano()
---
+++ tw_fa25/lava.gd
@@ -0,0 +1,26 @@
+extends Area2D
+class_name LavaSource
+
+@export var spread_time_og: float = 8
+@export var fire_scene: PackedScene
+@onready var space = get_world_2d().direct_space_state
+var timer: float = 0
+var spread_time: float
+
+func _ready() -> void:
+	spread_time= spread_time_og
+
+func _process(delta: float) -> void:
+	timer+=delta
+	if timer>=spread_time:
+		spread_time=spread_time_og*randf_range(0.8,5)
+		timer=0
+		spawn_fire_nearby()
+
+func spawn_fire_nearby() -> void:
+	# pick a random offset near lava
+	var offset = Vector2(randf_range(-64, 64), randf_range(-64, 64))
+	var fire_pos = global_position + offset
+	var fire = fire_scene.instantiate()
+	fire.global_position = fire_pos
+	get_tree().current_scene.add_child(fire)
