4fdffdbd76e9a4a43db170f06cb82f092a69b1d9
gloomybear300 <s2hill@ucsd.edu>
2025-10-22 11:28:19 -0700

    Revert "Updated UI for faith"
    This reverts commit b774ffaf10ac8a7a95490304aa8682f779ce376e.

--- tw_fa25/gui.gd
+++ tw_fa25/gui.gd
@@ -163,7 +163,6 @@ func tutorial_process():
 		"kill":
 			if has_killed:
 				set_tutorial_state("over")
-				faith_bar.position.x = 175;
 
 func check_hover_area():
 	var mouse_pos = game.get_global_mouse_position()
